ec8852e637647b89fabd945ee91684149a38eaa2
Peter Stritzinger <peter.satzinger@abf.at>
2020-08-26 08:52:24 +0200

    added base and customer entity

---
+++ code-java/product/src/main/java/com/kleenxcoder/product/base/model/entity/BaseUserEntity.java
@@ -0,0 +1,43 @@
+package com.kleenxcoder.product.base.model.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.Description;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Table(name = "user")
+@Description("user table")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@MappedSuperclass
+public class BaseUserEntity extends BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "active")
+    private Boolean active;
+}
---
+++ code-java/product/src/main/java/com/kleenxcoder/product/impl/model/entity/UserEntity.java
@@ -0,0 +1,12 @@
+package com.kleenxcoder.product.impl.model.entity;
+
+import javax.persistence.Entity;
+
+import com.kleenxcoder.product.base.model.entity.BaseUserEntity;
+
+@Entity
+public class UserEntity extends BaseUserEntity {
+
+	private static final long serialVersionUID = 1L;
+
+}
