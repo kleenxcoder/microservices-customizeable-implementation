6f75a99e1dd2a6473ac33d3fda71ffc4abdb848f
Peter Stritzinger <peter.satzinger@abf.at>
2020-08-27 16:38:52 +0200

    moved controller base methods to base class

---
+++ code-java/product/src/main/java/com/kleenxcoder/product/base/controller/BaseUserControllerImpl.java
@@ -0,0 +1,24 @@
+package com.kleenxcoder.product.base.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.kleenxcoder.product.base.model.entity.BaseUser;
+import com.kleenxcoder.product.base.model.repository.BaseUserRepository;
+import com.kleenxcoder.product.base.service.BaseUserService;
+
+public abstract class BaseUserControllerImpl<T extends BaseUser, R extends BaseUserRepository<T> , S extends BaseUserService<T, R> > {
+	
+	@Autowired
+	protected S service;
+	
+	@GetMapping("/findByEmail/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public T findByEmail(@PathVariable String email) {
+        return service.findByEmail(email);
+    }	
+
+}
